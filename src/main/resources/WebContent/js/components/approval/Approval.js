9a246a6f9154faf22c6bce5f4d7a36589d07b1b7
Nick <544436+Milchreis@users.noreply.github.com>
2018-06-02 23:30:53 +0200

    Image fitting, styling and animation, image magnification

--- src/main/resources/WebContent/js/components/approval/Approval.js
+++ src/main/resources/WebContent/js/components/approval/Approval.js
@@ -4,14 +4,20 @@ const Approval = Vue.component(
     <div id="approval">
         <!-- Show selected picture -->
         <div class="picPanel">
-            <img :src="selectedPic" v-if="selectedPic" class="selectedPic" />
+            <transition name="fadeIn">
+                <img :src="selectedPic" id="mainPic" 
+                    :data-zoom-image="selectedPic"
+                    :key="selectedPic" 
+                    v-if="selectedPic" 
+                    class="selectedPic" />
+            </transition>
         </div>
 
         <!-- List all elements -->
         <div class="pictureNavigation">
             <ul>
                 <li v-for="pic in pictures">
-                    <img :src="pic" class="preview" @click="onPictureSelect(pic)" />
+                    <img :src="pic" class="preview" @click="onPictureSelect(pic)" v-bind:class="{ isSelected: pic === selectedPic }" />
                 </li>
             </ul>
         </div>
@@ -51,31 +57,64 @@ const Approval = Vue.component(
 
         onPictureSelect: function(pic) {
             this.selectedPic = pic;
-        },
 
-    /*	refresh () {
-    		path = this.$route.params.albumpath;
-            if(path !== undefined) {
-        		var that = this;
-                new ComService().getAlbum(path, function(data) {
-                    that.album = data;
+            setTimeout(() => {
+                console.log($("#mainPic"));
+    
+                $("#mainPic").elevateZoom({
+                    zoomType: "lens",
+                    zoomLens: false,
+                    lensShape: "square",
+                    lensSize: 300
                 });
-            }
-    	},
-
-        open: function(albumName) {
-            router.push({ path: "/albums/"+albumName });
+            }, 500);
         },
 
-        goBack: function() {
-            this.$router.go(-1);
-        },*/
+        next: function() {
+			if(this.hasNext) {
+                index = this.pictures.indexOf(this.selectedPic);
+				this.selectedPic = this.pictures[index+1];
+			}
+		},
 
+		previous: function() {
+			if(this.hasPrevious) {
+				index = this.pictures.indexOf(this.selectedPic);
+				this.selectedPic = this.pictures[index-1];
+			}
+		},
     },
-    computed: {},
+    computed: {
+        hasNext: function() {
+			index = this.pictures.indexOf(this.selectedPic);
+			return index+1 < this.pictures.length;
+		},
+
+		hasPrevious: function() {
+			index = this.pictures.indexOf(this.selectedPic);
+			return index-1 >= 0;
+		},
+    },
     watch: {
 	},
 	created () {
         this.init();
+
+        var that = this;
+
+        // Add cursor key listeners for navigation
+        document.onkeydown = function (e) {
+            e = e || window.event;
+
+            // Right/Next
+            if(e.keyCode === 39 && that.hasNext) {
+                that.next();
+            }
+
+            // Left/Previous
+            if(e.keyCode === 37 && that.hasPrevious) {
+                that.previous();
+            }
+        };
   	}
 });
